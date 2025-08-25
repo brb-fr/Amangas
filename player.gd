e26427abb66b4e7e2b8761bf3dee722610bfd2a7
brb-fr <tt.brbfr@gmail.com>
2025-08-26 02:46:33 +0300

    first deploy

--- player.gd
+++ player.gd
@@ -5,4 +5,7 @@ func _physics_process(delta: float) -> void:
 	var inp = Input.get_vector("ui_left","ui_right","ui_up","ui_down") * speed
 	velocity = inp
 	move_and_slide()
-	$Sprite.flip_h = inp.x < 0
+	if inp.x < 0:
+		$Sprite.flip_h = true
+	else:
+		$Sprite.flip_h = false
