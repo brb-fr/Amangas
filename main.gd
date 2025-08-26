305b93e9a02b8496277ff6aa5f092b0403379d5e
brb-fr <tt.brbfr@gmail.com>
2025-08-26 17:22:30 +0300

    v3

---
+++ main.gd
@@ -0,0 +1,3 @@
+extends Node2D
+
+var options := {"host": 1, "players": [{}]}
--- player.gd
+++ player.gd
@@ -1,11 +1,48 @@
 extends CharacterBody2D
 
-@export var speed = 100
-func _physics_process(delta: float) -> void:
-	var inp = Input.get_vector("ui_left","ui_right","ui_up","ui_down") * speed
-	velocity = inp
+@export var speed := 130
+var last_horizontal = 0
+var last_vertical = 0
+
+func _physics_process(delta):
+	var left = Input.is_action_pressed("ui_left")
+	var right = Input.is_action_pressed("ui_right")
+	var up = Input.is_action_pressed("ui_up")
+	var down = Input.is_action_pressed("ui_down")
+
+	var horizontal = 0
+	var vertical = 0
+
+	if left and right:
+		horizontal = last_horizontal
+	elif left:
+		horizontal = -1
+	elif right:
+		horizontal = 1
+
+	if up and down:
+		vertical = last_vertical
+	elif up:
+		vertical = -1
+	elif down:
+		vertical = 1
+
+	# remember last pressed direction
+	if horizontal != 0:
+		last_horizontal = horizontal
+		$Sprite.flip_h = horizontal == -1
+	if vertical != 0:
+		last_vertical = vertical
+
+	velocity = Vector2(horizontal, vertical)
+	if velocity != Vector2.ZERO:
+		velocity = velocity.normalized() * speed
+
+	# move character
 	move_and_slide()
-	if inp.x < 0:
-		$Sprite.flip_h = true
+
+	# play animation based on **actual movement**
+	if velocity.length() > 0:
+		$Anims.play("walk")
 	else:
-		$Sprite.flip_h = false
+		$Anims.play("RESET")
