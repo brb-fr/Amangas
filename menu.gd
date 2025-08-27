007c7639d33f90d92edd6d55ab104dcd931391e0
brb-fr <tt.brbfr@gmail.com>
2025-08-27 16:21:40 +0300

    js loading anim

--- menu.gd
+++ menu.gd
@@ -6,6 +6,8 @@ const PORT = 22023
 func _ready() -> void:
 	multiplayer.connected_to_server.connect(func():
 		Server.rpc_id(1,"create_game","brbfr")
+		$Host/Icon/Tube/Loading.hide()
+		$Private/Icon/Tube/Loading.hide()
 	)
 
 
@@ -13,3 +15,4 @@ func _on_host_pressed() -> void:
 	var peer = WebSocketMultiplayerPeer.new()
 	peer.create_client(str("ws://%s"%ip))
 	multiplayer.multiplayer_peer = peer
+	$Host/Icon/Tube/Loading.show()
