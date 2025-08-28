ec472ec930cf372415de2f3a6c60b282d96ad1a2
brb-fr <tt.brbfr@gmail.com>
2025-08-28 19:23:08 +0300

    v3

--- main.gd
+++ main.gd
@@ -4,20 +4,37 @@ var players := []
 var options := {"host": 1}
 @export var player_scene: PackedScene
 
-
 func _ready() -> void:
-	Server.rpc_id(1, "host_joined_lobby")
+	var my_id = multiplayer.get_unique_id()
+	if not players.has(my_id):
+		players.append(my_id)
+		_add_player(my_id, true)
 	Server.player_request_join.connect(func(id:int):
-		var player = player_scene.instantiate()
-		player.name = str(id)
-		player.position = $Spawn.position
-		players.append(id)
-		$Players.call_deferred("add_child",player)
-		for player_id in players:
-			rpc_id(player_id,"set_players", players, options)
-	)
+		if not players.has(id):
+			players.append(id)
+			_add_player(id, false)
+		_sync_players_to_all())
 
-@rpc("authority","call_remote","reliable")
-func set_players(players_arr:Array,options_dict:Dictionary):
+	_sync_players_to_all()
+
+func _add_player(id:int, is_local_authority:bool) -> void:
+	if $Players.has_node(str(id)):
+		return
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	player.position = $Spawn.position
+	$Players.call_deferred("add_child", player)
+	if is_local_authority:
+		player.set_multiplayer_authority(id)
+		
+func _sync_players_to_all() -> void:
+	for player_id in players:
+		rpc_id(player_id, "set_players", players, options)
+@rpc("call_remote", "any_peer", "reliable")
+func set_players(players_arr:Array, options_dict:Dictionary):
 	options = options_dict
 	players = players_arr
+	for id in players_arr:
+		if not $Players.has_node(str(id)):
+			_add_player(id, multiplayer.get_unique_id() == id)
+			
--- player.gd
+++ player.gd
@@ -3,20 +3,21 @@ extends CharacterBody2D
 @export var speed := 130
 var last_horizontal = 0
 var last_vertical = 0
+@onready var anim = $Anims
+@onready var sprite = $Sprite
+
 
 func _enter_tree() -> void:
-	set_multiplayer_authority(int(name))
-
+	set_multiplayer_authority(name.to_int())
+	
 func _physics_process(delta):
-	if get_multiplayer_authority():
+	if is_multiplayer_authority():
 		var left = Input.is_action_pressed("ui_left")
 		var right = Input.is_action_pressed("ui_right")
 		var up = Input.is_action_pressed("ui_up")
 		var down = Input.is_action_pressed("ui_down")
-
 		var horizontal = 0
 		var vertical = 0
-
 		if left and right:
 			horizontal = last_horizontal
 		elif left:
@@ -30,23 +31,16 @@ func _physics_process(delta):
 			vertical = -1
 		elif down:
 			vertical = 1
-
-		# remember last pressed direction
 		if horizontal != 0:
 			last_horizontal = horizontal
-			$Sprite.flip_h = horizontal == -1
+			sprite.flip_h = horizontal == -1
 		if vertical != 0:
 			last_vertical = vertical
-
 		velocity = Vector2(horizontal, vertical)
 		if velocity != Vector2.ZERO:
 			velocity = velocity.normalized() * speed
-
-		# move character
 		move_and_slide()
-
-		# play animation based on **actual movement**
 		if velocity.length() > 0:
-			$Anims.play("walk")
+			anim.play("walk")
 		else:
-			$Anims.play("RESET")
+			anim.play("RESET")
